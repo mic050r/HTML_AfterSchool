fc5cc92e6d96ecb45c6616f23f6f25ea1101d59e
林지현 <103114387+mic050r@users.noreply.github.com>
2023-10-10 09:11:14 +0900

    html 방과후 코드

---
+++ day_0607/lib.js
@@ -0,0 +1,11 @@
+	<script type="text/javascript">
+		function over(obj) {
+			obj.src=."banana.jpg";
+		}
+		function out(obj) {
+			obj.src ="apple.jpg"
+		}
+		function click1(obj){
+			obj.src = "apple.jpg"
+		}
+	</script>
